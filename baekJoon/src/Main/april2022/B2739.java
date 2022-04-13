d899984219bf92e6753b2dbad2eb6c3aed613a95
LEE EUNJI <eunji6986@naver.com>
2022-04-14 00:34:12 +0900

    #2739 #10950

--- baekJoon/src/Main/april2022/B2739.java
+++ baekJoon/src/Main/april2022/B2739.java
@@ -1,10 +1,19 @@
 package Main.april2022;
 
+import java.util.Scanner;
+
 public class B2739 {
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		int n ;
+		n = sc.nextInt();
 		
-		
+		for(int i=1; i <=9; i++) {
+			
+			System.out.println(n +" * "+ i +" = "+ n*i);
+			
+		}
 		
 		
 	}
